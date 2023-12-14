d160e1eacff3e30d9c439de3f79ebf4b4c5905f7
Timothy Attah <timothyattah733@gmail.com>
2023-12-14 07:11:27 +0100

    Changed price details

--- src/pages/aircraft/modules/latestAircrafts/LatestAircraftSix.jsx
+++ src/pages/aircraft/modules/latestAircrafts/LatestAircraftSix.jsx
@@ -71,7 +71,7 @@ const LatestAircraftSix = () => {
         </Styles.AircraftTrendingDetailsTitle>
         <Styles.AircraftTrendingDetailsPrice>
           <FadeIn delay={0.4} direction='left' fullWidth padding>
-            Price: $489,000,000
+            Price: $89,000,000
           </FadeIn>
         </Styles.AircraftTrendingDetailsPrice>
       </div>
@@ -144,7 +144,7 @@ const LatestAircraftSix = () => {
               </Styles.AircraftTrendingMainDetailsOptions>
               <Styles.AircraftTrendingMainDetailsOptions>
                 <h6>Price:</h6>
-                <h6> $489,000,000</h6>
+                <h6> $89,000,000</h6>
               </Styles.AircraftTrendingMainDetailsOptions>
             </Styles.AircraftTrendingMainDetailsLeftBox>
           </Styles.AircraftTrendingMainDetailsBox>
